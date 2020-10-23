af7a61327de4def3f05d8ced3d4613b97eb63724
jeondaelyong <fldragonjun@gmail.com>
2020-10-23 19:07:18 +0900

    화면 초기화

--- py_pang/6_final.py
+++ py_pang/6_final.py
@@ -20,6 +20,16 @@ img_path = os.path.join(cur_path, 'images')
 
 # 이미지 불러오기
 background_img = pygame.image.load(os.path.join(img_path, 'background.png'))
+stage_img = pygame.image.load(os.path.join(img_path, 'stage.png'))
+stage_rect = stage_img.get_rect().size
+stage_height = stage_rect[1]
+
+character_img = pygame.image.load(os.path.join(img_path, 'character.png'))
+character_rect = character_img.get_rect().size
+character_width = character_rect[0]
+character_height = character_rect[1]
+character_pos_x = screen_width // 2 - character_width // 2
+character_pos_y = screen_height - stage_height - character_height
 
 # 게임 루프
 running = True
@@ -29,6 +39,8 @@ while running:
             running = False
 
     screen.blit(background_img, (0, 0))
+    screen.blit(stage_img, (0, screen_height - stage_height))
+    screen.blit(character_img, (character_pos_x, character_pos_y))
     pygame.display.update()
 
 # 파이게임 종료
